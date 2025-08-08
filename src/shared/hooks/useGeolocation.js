3f5514850e8323d58bdc9597d72baa899a5d2564
MavlonTurgunof <mavlondev2023@gmail.com>
2025-08-09 03:46:18 +0500

    updated

--- src/shared/hooks/useGeolocation.js
+++ src/shared/hooks/useGeolocation.js
@@ -1,10 +1,16 @@
+// useGeolocation.js
 import { useEffect, useState } from "react";
 
-export function useGeolocation() {
-  const [coords, setCoords] = useState({ lat: null, lon: null });
+export function useGeolocation(shouldFetch = true) {
+  const [coords, setCoords] = useState(() => {
+    const saved = localStorage.getItem("coords");
+    return saved ? JSON.parse(saved) : { lat: null, lon: null };
+  });
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!shouldFetch || coords.lat) return;
+
     if (!navigator.geolocation) {
       setError("Geolocation is not supported by your browser");
       return;
@@ -12,16 +18,18 @@ export function useGeolocation() {
 
     navigator.geolocation.getCurrentPosition(
       (pos) => {
-        setCoords({
+        const position = {
           lat: pos.coords.latitude,
           lon: pos.coords.longitude,
-        });
+        };
+        setCoords(position);
+        localStorage.setItem("coords", JSON.stringify(position));
       },
       (err) => {
         setError(err.message);
       }
     );
-  }, []);
+  }, [shouldFetch, coords.lat]);
 
   return { ...coords, error };
 }
